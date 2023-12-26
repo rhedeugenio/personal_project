948bc8a3dddc4d884581cf272a3f00fbcccc6cf0
John Rhed Eugenio <johnrhed.eugenio@gmail.com>
2023-12-25 23:15:56 -0600

    landing page almost complete

--- src/components/IntroDesc.tsx
+++ src/components/IntroDesc.tsx
@@ -4,11 +4,13 @@ import React from 'react'
 const IntroDesc = () => {
   return (
 
-    <div className="p-10 flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-15rem)] lg:flex-row bg-cover bg-sangoon-custom">
-        <div className="flex-1 flex items-center justify-center flex-col gap-4 text-white uppercase font-bold">
-            <h1 className="text-2xl text-center p-4 md:text-2xl xl:text-3xl">
-                This is a website for setting a coaching appointment <br/> related for Black Desert Online.
+    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-15rem)] lg:flex-row bg-cover bg-sangoon-custom">
+        <div className="flex-1 flex items-center justify-center flex-col gap-4 text-white uppercase font-bold backdrop-blur-sm">
+            <h1 className="text-2xl text-center p-4 md:text-2xl xl:text-3xl font-sans">
+                A Web Application for Scheduling Coaching Appointments <br/> Related To Black Desert Online.
             </h1>
+            <button className="p-3 bg-white text-black outline outline-blue-600 outline outline-offset-2 rounded-lg
+            hover:bg-gray-200 active:bg-gray-300">Schedule Appointment</button>
         </div>
     </div>
   )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,36 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className="h-20 text-white p-0 flex items-center border-b-4 border-b-black bg-gray-600 uppercase lg:px-10 xl:px-20 gap-4">
-
-        <div className="flex-1 flex justify-self-center gap-3">
-            <Link href="/" className="border-2 rounded-2xl p-2 hover:bg-gray-400">Schedule</Link>
-            <Link href="/" className="border-2 rounded-2xl p-2 hover:bg-gray-400">Contact</Link>
-            <Link href="/" className="border-2 rounded-2xl p-2 hover:bg-gray-400">Login</Link>
-        </div>
+    <div className="h-20 text-white items-center flex justify-between bg-gray-300 uppercase lg:px-10 xl:px-20 font-sans font-bold">
+      <div className="flex-1 flex justify-start">
+        {/* 
         
-        {/*Website Title and Home Link*/}
-        <div className="text-2xl flex-1 flex justify-center">
-            <Link href="/">Coaching Appointment</Link>
-        </div>
-        <div className="flex-1 flex justify-end gap-3"></div>
+        <Link href="/" className="flex pr-4">
+            <Image src='/bdo_logo.jpg' alt='' width={60} height={20} className=""/>
+          </Link>
+          <Link href="/" className="p-4 text-black border-l-2 border-r-2 hover:text-blue-600 active:text-blue-400">Schedule</Link>
+          <Link href="/" className="p-4 text-black border-r-2 hover:text-blue-600 active:text-blue-400">Contact</Link>
+        */}
+          <Link href="/" className="pr-4">
+            <Image src='/bdo_logo.jpg' alt='' width={60} height={20} className=""/>
+          </Link>
+          <Link href="/" className="p-4 text-black border-l-2 border-r-2 hover:text-blue-600 active:text-blue-400">Schedule</Link>
+          <Link href="/" className="p-4 text-black border-r-2 hover:text-blue-600 active:text-blue-400">Contact</Link>
+          <Link href="/" className="p-4 text-black  border-r-2 hover:text-blue-600 active:text-blue-400">Location</Link>
+
+      </div>
+        
+      {/*Website Title and Home Link*/}
+      <div className="text-2xl text-black flex flex-1 justify-center">
+          <Link href="/">Appointment</Link>
+      </div>
+      <div className="flex flex-1 justify-end">
+        <Link href="/" className="p-4 text-black border-l-2 border-r-2 hover:text-blue-600 active:text-blue-400">Documentation</Link>
+        <Link href="/" className="p-4 text-black border-r-2 hover:text-blue-600 active:text-blue-400">Login</Link>
+      </div>
     </div>
     
   )
